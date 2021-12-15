60fb7f9753920b9fd30fbf43c776d900e27cdff7
Umair Rasheed <34655240+umair3082@users.noreply.github.com>
2021-12-15 14:24:43 +0500

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,5 @@
+//Go to Load function of Form
+// then past the code below
+ImageList imageList1 = new ImageList();
+            cls_change_supergrid_img_size gvcs = new cls_change_supergrid_img_size();
+            gvcs.InitializeGrid(this.superGridControl1, "pic", ImageSizeMode.Zoom, imageList1);
